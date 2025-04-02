ab21662ea8954e72114718f859718c55eca090bf
aiturbidemil <aiturbide@milaboratories.com>
2025-04-02 12:05:48 +0200

    settings in main

--- ui/src/pages/MainPage.vue
+++ ui/src/pages/MainPage.vue
@@ -1,18 +1,11 @@
 <script setup lang="ts">
 import '@milaboratories/graph-maker/styles';
-import { PlAccordionSection, PlAlert, PlBlockPage, PlBtnGhost, PlDropdownRef, PlLogView, PlMaskIcon24, PlNumberField, PlRow, PlSlideModal } from '@platforma-sdk/ui-vue';
+import { PlAccordionSection, PlAlert, PlBlockPage, PlDropdownRef, PlNumberField, PlRow } from '@platforma-sdk/ui-vue';
 import { useApp } from '../app';
-import { reactive } from 'vue';
 import { plRefsEqual } from '@platforma-sdk/model';
 import type { PlRef } from '@platforma-sdk/model';
 
 const app = useApp();
-
-const data = reactive<{
-  settingsOpen: boolean;
-}>({
-  settingsOpen: app.model.args.countsRef === undefined,
-});
 
 function setInput(inputRef?: PlRef) {
   app.model.args.countsRef = inputRef;
@@ -26,58 +19,46 @@ function setInput(inputRef?: PlRef) {
 
 <template>
   <PlBlockPage>
-    <template #title>Dimensionality Reduction</template>
-    <template #append>
-      <PlBtnGhost @click.stop="() => data.settingsOpen = true">
-        Settings
-        <template #append>
-          <PlMaskIcon24 name="settings" />
-        </template>
-      </PlBtnGhost>
-    </template>
-    <PlLogView :log-handle="app.model.outputs.dimReductionLog" label="Log"/>
-
-    <PlSlideModal v-model="data.settingsOpen">
-      <template #title>Settings</template>
-      <PlDropdownRef
-        v-model="app.model.args.countsRef" :options="app.model.outputs.countsOptions"
-        label="Select dataset"
-        clearable @update:model-value="setInput"
-      />
-      <!-- Content hidden until you click ADVANCED SETTINGS -->
-      <PlAccordionSection label="ADVANCED SETTINGS">
-        <PlRow>
-          <PlNumberField
-            v-model="app.model.args.nPCs"
-            label="N PCs" :minValue="20" :step="1"
-          >
-            <template #tooltip>
-              Select number of Principal Components (PCs) to use.
-            </template>
-          </PlNumberField>
-          <PlNumberField
-            v-model="app.model.args.nNeighbors"
-            label="N Neighbors" :minValue="5" :step="1"
-          >
-            <template #tooltip>
-              Select number of neighbors for the UMAP algorithm.
-            </template>
-          </PlNumberField>
-        </PlRow>
-        <!-- Add warnings if selected parameters are out of most commonly used bounds -->
-        <PlAlert v-if="app.model.args.nPCs < 30" type="warn">
-          {{ "Warning: The selected number of PCs is out of the most commonly used range (30-50)" }}
-        </PlAlert>
-        <PlAlert v-if="app.model.args.nPCs > 50" type="warn">
-          {{ "Warning: The selected number of PCs is out of the most commonly used range (30-50)" }}
-        </PlAlert>
-        <PlAlert v-if="app.model.args.nNeighbors < 10" type="warn">
-          {{ "Warning: The selected number of neighbors is out of the most commonly used range (10-30)" }}
-        </PlAlert>
-        <PlAlert v-if="app.model.args.nNeighbors > 30" type="warn">
-          {{ "Warning: The selected number of neighbors is out of the most commonly used range (10-30)" }}
-        </PlAlert>
-      </PlAccordionSection>
-    </PlSlideModal>
+    <template #title>Settings</template>
+    <PlDropdownRef
+      v-model="app.model.args.countsRef" :options="app.model.outputs.countsOptions"
+      :style="{ width: '320px' }"
+      label="Select dataset"
+      clearable @update:model-value="setInput"
+    />
+    <!-- Content hidden until you click ADVANCED SETTINGS -->
+    <PlAccordionSection :style="{ width: '320px' }" label="ADVANCED SETTINGS">
+      <PlRow>
+        <PlNumberField
+          v-model="app.model.args.nPCs"
+          label="N PCs" :minValue="20" :step="1"
+        >
+          <template #tooltip>
+            Select number of Principal Components (PCs) to use.
+          </template>
+        </PlNumberField>
+        <PlNumberField
+          v-model="app.model.args.nNeighbors"
+          label="N Neighbors" :minValue="5" :step="1"
+        >
+          <template #tooltip>
+            Select number of neighbors for the UMAP algorithm.
+          </template>
+        </PlNumberField>
+      </PlRow >
+      <!-- Add warnings if selected parameters are out of most commonly used bounds -->
+      <PlAlert v-if="app.model.args.nPCs > 20 && app.model.args.nPCs < 30" type="warn">
+        {{ "Warning: The selected number of PCs is out of the most commonly used range (30-50)" }}
+      </PlAlert>
+      <PlAlert v-if="app.model.args.nPCs > 50" type="warn">
+        {{ "Warning: The selected number of PCs is out of the most commonly used range (30-50)" }}
+      </PlAlert>
+      <PlAlert v-if="app.model.args.nNeighbors > 5 && app.model.args.nNeighbors < 10" type="warn">
+        {{ "Warning: The selected number of neighbors is out of the most commonly used range (10-30)" }}
+      </PlAlert>
+      <PlAlert v-if="app.model.args.nNeighbors > 30" type="warn">
+        {{ "Warning: The selected number of neighbors is out of the most commonly used range (10-30)" }}
+      </PlAlert>
+    </PlAccordionSection>
   </PlBlockPage>
 </template>
